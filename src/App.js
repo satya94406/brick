797bf15937cc6baba8114a63ab00c7c0f8d590be
Satya960 <satya9604166@gmail.com>
2023-11-11 00:54:50 +0530

    name has been changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Brickssssss...
+          Brickssssss..
         </p>
         <a
           className="App-link"
